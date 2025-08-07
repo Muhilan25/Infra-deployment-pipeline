9312ffb458b3ebc0ec1841a89621e9dfac2bbcf6
muhilan <muhilanrangaraj99@gmail.com>
2025-08-07 13:39:15 +0530

    destroy ec2

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,4 +1,4 @@
-module "vpc" {
+/*module "vpc" {
   source = "../../modules/vpc"
   vpc_cidr = "10.0.0.0/16"
   subnet_cidr = "10.0.1.0/24"
@@ -13,3 +13,4 @@ module "ec2" {
    ec2_count = 2
   env = "dev"
 }
+*/
